0433f68f37a71cb4511f3e62bd11a91c05a1273e
tenjumh <tenjumh@gmail.com>
2020-04-17 18:28:30 +0900

    task NLP

--- Scikit-Learn_TF-IDF/2019711752_윤민형_TFIDF.py
+++ Scikit-Learn_TF-IDF/2019711752_윤민형_TFIDF.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[16]:
+# In[37]:
 
 
 import json
@@ -11,7 +11,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[17]:
+# In[38]:
 
 
 komoran = Komoran("EXP")
@@ -20,7 +20,7 @@ with open('C:/Users/fxk/PycharmProjects/tenjumh/Natural_Language_Processing/NLP_
     data = json.load(read_file)
 
 
-# In[18]:
+# In[39]:
 
 
 tfidf = []
@@ -30,29 +30,34 @@ for idx, key in enumerate(data.keys()):
         komoran_text = ' '.join(komoran.get_morphes_by_tags(message[1],
                                                    tag_list=['NNP', 'NNG', 'VV']))
         text.append(komoran_text)
-    if text != []:
+    if len(text) == 0:
+        continue
+    else:
+        num = idx + 1
         tfidfvect = TfidfVectorizer(token_pattern='(?u)\\b\\w+\\b', stop_words=None)
         tfidfvect.fit_transform(text)
         tfidflist = tfidfvect.transform(text).toarray().tolist()
         voca = tfidfvect.vocabulary_
         voca = sorted(voca)
+        tfidf.append('(대화{})'.format(num) + '\n')
         tfidf.append(tfidflist)
 
 
-# In[20]:
+# In[40]:
 
 
 save_file_name = '2019711752_윤민형_TFIDF'
 with open('./'+save_file_name+'.txt', 'w', encoding='utf-8', newline='') as writer_text:
     for idx, i in enumerate(tfidf):
-        num = '(대화{})'.format(idx+1)
-        writer_text.writelines(num + '\n')
-        for j in i:
-            for k in j:
-                k = round(k, 4)
-                writer_text.writelines(str(k) + '\t')
+        if idx % 2 == 0:
+            writer_text.writelines(i)
+        else:
+            for j in i:
+                for k in j:
+                    k = round(k, 4)
+                    writer_text.writelines(str(k) + '\t')
+                writer_text.writelines('\n')
             writer_text.writelines('\n')
-        writer_text.writelines('\n')
     print("[저장 완료]")
 
 
